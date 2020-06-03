10561753ff66e0510add8645fb2d22981b0e714e
Ekow Adu <ekowsadu18@gmail.com>
2020-06-03 05:54:44 +0000

    Updates

---
+++ precache-manifest.1b5e4ea6b963f1cda91b087ff3d3866d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "041d43fe7a52741fc9cca28f19d15b60",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d23acd2e5f013d4eac9c",
+    "url": "/monsters-rolodex/static/css/main.3b71127b.chunk.css"
+  },
+  {
+    "revision": "12240f73676d8f2fbd85",
+    "url": "/monsters-rolodex/static/js/2.a3c82928.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a3c82928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d23acd2e5f013d4eac9c",
+    "url": "/monsters-rolodex/static/js/main.b3f1027b.chunk.js"
+  },
+  {
+    "revision": "6062e4a584c29c6e0372",
+    "url": "/monsters-rolodex/static/js/runtime-main.e3d252a9.js"
+  }
+]);
